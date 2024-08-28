809548598b2801204f9aafa91938bd674f420dda
Prince <princevora684@gmail.com>
2024-08-28 12:39:57 +0530

    Make classes

---
+++ app/Classes/DnsHandler.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Classes;
+
+class DnsHandler
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
---
+++ app/Classes/Handler.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Classes;
+
+class Handler
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
